e4450f6c2c361c637726d938204fd385e1ff916b
rou00 <rouaneKaghim17@gmail.com>
2026-02-01 14:34:41 +0100

    Setup Project Bank System App using SpringBoot, JPA, Hibernate, Oracle DB

---
+++ src/test/java/com/example/Bank/System/App/BankSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bank.System.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
